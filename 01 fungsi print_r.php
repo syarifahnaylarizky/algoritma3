33aa266a56d894fbacbf7984cdaa1ed650e203f6
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-29 10:29:45 +0700

    membuat fungsi print

--- 01 fungsi print_r.php
+++ 01 fungsi print_r.php
@@ -1,6 +1,9 @@
 <?php
+
 $nama = "syarifah nayla rizky";
 print_r($nama);
+print ("<p>");
 $buah = array("apel", "pisang", "ceri");
 print_r($buah);
+echo "<p><strong> by syarifah nayla rizky</strong>";
 ?>
