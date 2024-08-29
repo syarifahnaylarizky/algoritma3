f503f1ddf703349880f31fc90459e16495ae8244
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-29 12:39:53 +0700

    menggunakan var_dump pre

--- 06 menggunakan var dump dengan pre.php
+++ 06 menggunakan var dump dengan pre.php
@@ -3,5 +3,5 @@ $variabel_array = array("mangga", "apel", "durian");
 echo "</pre>";
 var_dump($variabel_array);
 echo "</pre>";
-print_r("<p><strong> by: syarifah nayla rizky </strong>");
+print_r("<p><strong> by: syarifah naylaa rizky </strong>");
 ?>
