41d06477a79f670253fc4dbd044bfce0c0529467
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-29 12:51:42 +0700

    membuat of print_r

--- 10 example of print_r.php
+++ 10 example of print_r.php
@@ -1,6 +1,6 @@
 <?php
 $array = array(
-    'name' => 'John Doe',
+    'name' => 'nayla',
     'age' => 30,
     'skills' => array('PHP','JavaScript','HTML')
 );
@@ -10,7 +10,7 @@ print_r($array);
 print "<p>";
 
 $Array = array(
-    'name]' => 'John Doe',
+    'name]' => 'nayla',
     'age' =>30,
     'skills' => Array('PHP', 'JavaScript', 'HTML')
 );
