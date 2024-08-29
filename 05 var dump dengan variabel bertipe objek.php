0fa18ad25e3c15a81445c3a9a2ceaf2e147b6f59
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-29 12:36:01 +0700

    var_dump objek

--- 05 var dump dengan variabel bertipe objek.php
+++ 05 var dump dengan variabel bertipe objek.php
@@ -9,5 +9,5 @@ $motor_baru->merk = "vario";
 $motor_baru->model = "scoopy";
 
 var_dump($motor_baru);
-print_r ("<p><strong>by :syarifah nayla rizky");
+print_r ("<p><strong>by :syarifah nayla rizkyy");
 ?>
