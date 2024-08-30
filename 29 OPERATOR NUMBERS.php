65eb522bbb1b846f28fa28217781099e1baa21f2
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-30 18:02:28 +0700

    operator numbers pembulatan angka

---
+++ 29 OPERATOR NUMBERS.php
@@ -0,0 +1,22 @@
+<?php
+
+// round()
+echo round(3.4); // Hasil: 3
+echo "<br>";
+echo round(3.5); // Hasil: 4
+echo "<br>";
+echo round(3.6); // Hasil: 4
+echo "<br>";
+echo round(3.678, 2); // Hasil: 3.68
+echo "<hr>";
+// ceil()
+echo ceil(3.3); // Hasil: 4
+echo "<br>";
+echo ceil(3.9); // Hasil: 4
+echo "<hr>";
+// floor()
+echo floor(3.3); // Hasil: 3
+echo "<br>";
+echo floor(3.9); // Hasil: 3
+echo "<hr>";
+print_r("<p><strong>by :syarifah nayla rizkyy");
