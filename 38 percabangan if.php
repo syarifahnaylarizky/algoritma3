1723716bf14b7e0ac901e4047350c01505f898fa
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-30 18:32:56 +0700

    membuat percabangan if php

---
+++ 38 percabangan if.php
@@ -0,0 +1,13 @@
+<?php
+<?php
+
+if (<kondisi>){
+    // eksekusi kode ini
+}
+<?php
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah!";
+}
+print_r("<p><strong>by :syarifah nayla rizkyy");
