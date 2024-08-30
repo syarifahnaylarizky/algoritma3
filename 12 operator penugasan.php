358a59875b5b1ee2f42db65022dbcdb2eeb81315
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-30 09:25:10 +0700

    membuat operator aritmatika

---
+++ 12 operator penugasan.php
@@ -0,0 +1,21 @@
+<?php 
+
+$speed = 83;
+$speed2 = 10;
+
+// ini opertor aritmatika
+$speed = $speed + 10;
+
+// maka nilai speed akan samadengan 83 + 10 = 93
+
+// ini operator penugasan
+$speed += 10;
+
+// sekarang nilai speed akan menjadi 93 + 10 = 103
+print($speed)."<hr>";
+
+$speed = $speed + 10;
+// dan
+$speed += 10;
+
+print($speed2);
