0aaf4b1aa31249c28eecb69f3cac1991a5b30252
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-30 18:18:01 +0700

    operasi matematika dasar

---
+++ 33 operasi matematika dasar.php
@@ -0,0 +1,16 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+
+// Penambahan
+echo $angka1 + $angka2; // Hasil: 15
+
+// Pengurangan
+echo $angka1 - $angka2; // Hasil: 5
+
+// Perkalian
+echo $angka1 * $angka2; // Hasil: 50
+
+// Pembagian
+echo $angka1 / $angka2; // Hasil: 2
+print_r("<p><strong>by :syarifah nayla rizkyy");
