3c16e51ef67649b46a1c7884a2a26e1f96c4f809
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-30 11:02:41 +0700

    membuat trim itrim&rtrim

---
+++ 22 ucwords dan lcfirst.php
@@ -0,0 +1,4 @@
+<?php
+echo ucwords("hello world!"); // Outputs: "Hello World!"
+echo lcfirst("Hello"); // Outputs: "hello"
+print_r("<p><strong>by :syarifah nayla rizkyy");
---
+++ 23 trim, ltrim, dan rtrim.php
@@ -0,0 +1,6 @@
+<?php
+$text = " Hello World! ";
+echo "[" . trim($text) . "]"; // Outputs: "[Hello World!]"
+echo "[" . ltrim($text) . "]"; // Outputs: "[Hello World! ]"
+echo "[" . rtrim($text) . "]"; // Outputs: "[ Hello World!]"
+print_r("<p><strong>by :syarifah nayla rizkyy");
